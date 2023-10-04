fe64ce3c28191a87c260f0c7fdbab17a321928ff
Jaime-SC <jaimeqsanchezc@gmail.com>
2023-10-04 02:45:46 -0300

    Flutter Web Enable

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,14 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:taller_mecanico/pages/login_page.dart';
 
-
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(const MyApp());
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -42,4 +44,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
