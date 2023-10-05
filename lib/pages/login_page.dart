6805952b5c9405adca9e925760db6f54c7b12302
seba501q <sebastian.anticoy@gmail.com>
2023-10-05 01:31:26 -0300

    cambio_login

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,7 +1,7 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:taller_mecanico/pages/home_page.dart';
-import '../widgets/app_colors.dart'; // Importa el archivo app_colors.dart
+import '../widgets/app_colors.dart';
 import '../widgets/reusable_widget.dart';
 
 class LoginPage extends StatefulWidget {
@@ -15,24 +15,66 @@ class _LoginPageState extends State<LoginPage> {
   TextEditingController _passwordTextController = TextEditingController();
   TextEditingController _emailTextController = TextEditingController();
 
+  // Variables para controlar la visibilidad de la notificación personalizada
+  bool _showCustomNotification = false;
+  String _customNotificationMessage = "";
+  Color? _customNotificationBackgroundColor;
+
+  // Función para mostrar una notificación personalizada en el centro de la pantalla
+void _showCustomNotificationMessage(String message, {Color? backgroundColor}) {
+  setState(() {
+    _customNotificationMessage = message;
+    _customNotificationBackgroundColor = backgroundColor ?? Colors.green; // Usar verde como color predeterminado si no se proporciona uno
+    _showCustomNotification = true;
+    });
+
+    // Ocultar la notificación después de unos segundos
+    Future.delayed(Duration(seconds: 5), () {
+      setState(() {
+        _showCustomNotification = false;
+      });
+    });
+  }
+
+  // Función para mostrar una alerta de inicio de sesión exitoso
+  void _showLoginSuccessAlert() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Inicio de sesión exitoso"),
+          content: Text("Direccionando a pagina home...."),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Cierra la alerta
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => HomePage(),
+                  ),
+                );
+              },
+              child: Text("Continuar"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: buildGradientContainer(
         SingleChildScrollView(
           child: Padding(
-            padding: EdgeInsets.fromLTRB(
-              20,
-              0,
-              //MediaQuery.of(context).size.height * 0.2,
-              20,
-              0,
-            ),
+            padding: EdgeInsets.fromLTRB(20,0,20,0,),
             child: Column(
               children: <Widget>[
                 logoWidget("assets/images/logo1.png"),
                 const SizedBox(
-                  height: 30,
+                  height: 5,
                 ),
                 Container(
                   decoration: BoxDecoration(
@@ -40,17 +82,15 @@ class _LoginPageState extends State<LoginPage> {
                     borderRadius: BorderRadius.circular(10),
                     boxShadow: [
                       BoxShadow(
-                        color:
-                            Colors.black.withOpacity(0.5), // Color de la sombra
-                        spreadRadius: 1.25, // Cuánto se extiende la sombra
-                        blurRadius: 7, // Cuánto se difumina la sombra
-                        offset: Offset(
-                            0, 3), // Offset de la sombra (horizontal, vertical)
+                        color: Colors.black.withOpacity(0.5),
+                        spreadRadius: 1.25,
+                        blurRadius: 7,
+                        offset: Offset(0, 3),
                       ),
                     ],
                   ),
                   child: Padding(
-                    padding: const EdgeInsets.all(50),
+                    padding: const EdgeInsets.all(25),
                     child: Column(
                       children: [
                         reusableTextField(
@@ -77,12 +117,10 @@ class _LoginPageState extends State<LoginPage> {
                                   email: _emailTextController.text,
                                   password: _passwordTextController.text)
                               .then((value) {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => HomePage()));
+                            _showLoginSuccessAlert(); // Mostrar la alerta de inicio de sesión exitoso
+                            // _showCustomNotificationMessage("Inicio de sesión exitoso");
                           }).onError((error, stackTrace) {
-                            print("Error ${error.toString()}");
+                            _showCustomNotificationMessage("Error al iniciar sesión. Verifica credenciales y vuelve a intentar.", backgroundColor: const Color.fromARGB(255, 227, 89, 79));
                           });
                         }),
                         opcionRegistro(context),
@@ -90,12 +128,25 @@ class _LoginPageState extends State<LoginPage> {
                     ),
                   ),
                 ),
+                // Widget de notificación personalizada
+                if (_showCustomNotification)
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 16),
+                    decoration: BoxDecoration(
+                      color: _customNotificationBackgroundColor,
+                      borderRadius: BorderRadius.circular(10), // Bordes redondeados
+                    ),
+                    padding: EdgeInsets.all(16),
+                    child: Text(
+                      _customNotificationMessage,
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
               ],
             ),
           ),
         ),
-        AppColors
-            .colorBase, // Utiliza la lista de colores desde app_colors.dart
+        AppColors.colorBase,
       ),
     );
   }
